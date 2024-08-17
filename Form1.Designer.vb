d34294dd435750c7f1e6cef6e7b469551c38a80c
hkearn777 <howard@kearney.name>
2024-08-16 22:30:54 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtInputFile = New System.Windows.Forms.TextBox()
+        Me.txtOutputFile = New System.Windows.Forms.TextBox()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtInputFile
+        '
+        Me.txtInputFile.Location = New System.Drawing.Point(36, 24)
+        Me.txtInputFile.Name = "txtInputFile"
+        Me.txtInputFile.Size = New System.Drawing.Size(684, 23)
+        Me.txtInputFile.TabIndex = 0
+        Me.txtInputFile.Text = "C:\Users\Kearney\Documents\StTammany\2024-08-01-St Tammany.csv"
+        '
+        'txtOutputFile
+        '
+        Me.txtOutputFile.Location = New System.Drawing.Point(36, 63)
+        Me.txtOutputFile.Name = "txtOutputFile"
+        Me.txtOutputFile.Size = New System.Drawing.Size(686, 23)
+        Me.txtOutputFile.TabIndex = 1
+        Me.txtOutputFile.Text = "C:\Users\Kearney\Documents\StTammany\Output.csv"
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(37, 103)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 2
+        Me.btnGo.Text = "GO"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(127, 103)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 147)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.txtOutputFile)
+        Me.Controls.Add(Me.txtInputFile)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInputFile As TextBox
+    Friend WithEvents txtOutputFile As TextBox
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnClose As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.IO
+
+Public Class Form1
+  ' take an address csv file and combine same <streetaddress1> + <zipcode> into a single row
+  '  and combine the name values to <FirstName> (first row) + " / " <FirstName> (second row) , etc.
+  ' The input file should already be sorted in StreetAddress1, Zipcode5, and Zipcode4
+
+  Dim swOutput As StreamWriter = Nothing
+  ' column positions
+  Dim FirstName As Integer = 0
+  Dim MiddleName As Integer = 1
+  Dim LastName As Integer = 2
+  Dim Address1 As Integer = 3
+  Dim City As Integer = 4
+  Dim USState As Integer = 5
+  Dim ZipCode5 As Integer = 6
+  Dim ZipCode4 As Integer = 7
+  Dim Sex As Integer = 8
+  Dim Race As Integer = 9
+  Dim Age As Integer = 10
+  Dim Registration_Date As Integer = 11
+  Dim Registration_Number As Integer = 12
+  Dim Phone As Integer = 13
+  Dim LastVoted As Integer = 14
+
+  Dim Delimiter As String = ","
+
+
+  Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+    Me.Close()
+  End Sub
+
+  Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+    If Not File.Exists(txtInputFile.Text) Then
+      MessageBox.Show("Can't find input file")
+      Exit Sub
+    End If
+    If File.Exists(txtOutputFile.Text) Then
+      If MessageBox.Show("Verify Overwrite", "Output already exists; Okay to overwrite?", MessageBoxButtons.OKCancel) = vbCancel Then
+        Exit Sub
+      End If
+    End If
+
+
+    'open the output file and wipeout previous
+    Dim OutputFileName As String = txtOutputFile.Text
+    swOutput = My.Computer.FileSystem.OpenTextFileWriter(OutputFileName, False)
+
+    ' load the csv into an array
+    Dim rows As String() = File.ReadAllLines(txtInputFile.Text)
+
+    If rows.Count = 0 Then
+      MessageBox.Show("CSV file is empty")
+      Exit Sub
+    End If
+    If rows.Count = 1 Then
+      MessageBox.Show("Only 1 record on CSV; presume a header record")
+      Exit Sub
+    End If
+
+    Dim duplicateRows As Integer = 0
+
+    ' process through the csv (skip header), creating output
+    For row As Integer = 1 To rows.Count - 1
+      ' split the row into columns
+      Dim columns As String() = rows(row).Split(",")
+      ' At last row on file? just write that out
+      If row = rows.Count - 1 Then
+        Call WriteTheRow(columns)
+        Exit For
+      End If
+      ' check if next record's key is same as this one.
+      duplicateRows = 0
+      For nextrow As Integer = row + 1 To rows.Count - 1
+        Dim nextColumns As String() = rows(nextrow).Split(Delimiter)
+        If nextColumns(Address1) = columns(Address1) And
+           nextColumns(ZipCode5) = columns(ZipCode5) And
+           nextColumns(ZipCode4) = columns(ZipCode4) Then
+          columns(FirstName) &= " / " & nextColumns(FirstName)
+          duplicateRows += 1
+        End If
+      Next
+      Call WriteTheRow(columns)
+      row += duplicateRows
+    Next
+
+    swOutput.Close()
+    MessageBox.Show("Process Complete.")
+  End Sub
+  Sub WriteTheRow(ByRef columns As String())
+    Try
+      swOutput.WriteLine(columns(FirstName) & Delimiter &
+                         columns(LastName) & Delimiter &
+                         columns(Address1) & Delimiter &
+                         columns(City) & Delimiter &
+                         columns(USState) & Delimiter &
+                         columns(ZipCode5) & "-" &
+                         columns(ZipCode4))
+
+    Catch ex As Exception
+      MessageBox.Show(ex.Message, "Error Writing to Output CSV")
+      Me.Close()
+    End Try
+  End Sub
+End Class
